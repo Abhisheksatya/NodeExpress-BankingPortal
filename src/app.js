8d8cfd0f2ec2144b756310dbde954e9294d44e5e
Abhishek Sharma <abhisatya23@gmail.com>
2020-07-31 12:49:20 +0530

    step3 complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ const app=express();
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'/public/')));
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts}));
+
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 app.listen(3000,function(){
     console.log('PS Project Running on port 3000!');
 })
